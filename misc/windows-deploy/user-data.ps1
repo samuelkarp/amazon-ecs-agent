f3a5b75c1a1afdaf363a629a1c58df71f2803da1
James Straub <jsstraub@amazon.com>
2016-11-12 17:08:13 -0800

    Changed md5 to fetch from URL.

--- misc/windows-deploy/user-data.ps1
+++ misc/windows-deploy/user-data.ps1
@@ -1,21 +1,20 @@
 <powershell>
-## The string 'windows' shoud be replaced with 
+## The string 'windows' shoud be replaced with
 ## template variable for cluster name.
 
 # Set agent env variables for the Machine context (durrable)
 [Environment]::SetEnvironmentVariable("ECS_CLUSTER", "windows", "Machine")
 [Environment]::SetEnvironmentVariable("ECS_ENABLE_TASK_IAM_ROLE", "false", "Machine")
 $agentVersion = '1.14.0-1.windows.1'
-$agentZipHash = '7748b1d3c73d5211e150db746cdec5bf'
-$agentZipUri = "https://s3-us-west-2.amazonaws.com/windows-agent/ecs-agent-windows-$agentVersion.zip"
+$agentZipUri = "https://s3.amazonaws.com/amazon-ecs-agent/ecs-agent-windows-${agentVersion}.zip"
 
-
-### --- Nothing user configureable after this point --- 
+### --- Nothing user configureable after this point ---
 $ecsExeDir = "$env:ProgramFiles\Amazon\ECS"
 $zipFile = "$env:TEMP\ecs-agent.zip"
 
 ### Get the files from S3
 Invoke-RestMethod -OutFile $zipFile -Uri $agentZipUri
+$agentZipHash = ((Invoke-RestMethod -Uri "$agentZipUri.md5").Split(" "))[0]
 
 ## MD5 Checksum
 $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
