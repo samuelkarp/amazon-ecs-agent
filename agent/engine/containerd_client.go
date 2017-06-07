6ae883eca2d17150c86ac0ae3dbd5ee0ba1d7db6
Samuel Karp <skarp@amazon.com>
2017-06-07 12:00:17 -0700

    wip: containerd: skeleton create

--- agent/engine/containerd_client.go
+++ agent/engine/containerd_client.go
@@ -1,8 +1,9 @@
 package engine
 
 import (
-	"errors"
 	"time"
+
+	"github.com/pkg/errors"
 
 	"golang.org/x/net/context"
 
@@ -23,6 +24,12 @@ func (unimplementedError) ErrorName() string {
 }
 
 var unimplemented = unimplementedError{errors.New("unimplemented")}
+
+const (
+	containerdImage = "docker.io/library/busybox:latest"
+
+	ecsNamespace = "ecs-test"
+)
 
 func NewContainerdClient() (DockerClient, error) {
 	client, err := containerd.New("/tmp/containerd.sock")
@@ -60,19 +67,47 @@ func (c *containerdClient) ContainerEvents(ctx context.Context) (<-chan DockerCo
 }
 
 func (c *containerdClient) PullImage(image string, authData *api.RegistryAuthenticationData) DockerContainerMetadata {
-	ctx := namespaces.WithNamespace(context.TODO(), "ecs-test")
-	pullResponse, err := c.client.Pull(ctx, "docker.io/library/redis:alpine", containerd.WithPullUnpack)
+	ctx := namespaces.WithNamespace(context.TODO(), ecsNamespace)
+	image = containerdImage // TODO
+	seelog.Debugf("Pulling %s.  No progress will be shown.", image)
+	pullResponse, err := c.client.Pull(ctx, image, containerd.WithPullUnpack)
 	if err != nil {
 		return DockerContainerMetadata{Error: CannotPullContainerError{err}}
 	}
 	seelog.Debug("Pulled", pullResponse)
 	// 2017-06-07T04:32:26Z [DEBUG] Pulled&{0xc4201d4520 {docker.io/library/redis:alpine {application/vnd.docker.distribution.manifest.v2+json sha256:03789f402b2ecfb98184bf128d180f398f81c63364948ff1454583b02442f73b 1568 [] map[] <nil>}}}
 
-	return DockerContainerMetadata{Error: unimplemented}
+	return DockerContainerMetadata{}
 }
 
-func (c *containerdClient) CreateContainer(*docker.Config, *docker.HostConfig, string, time.Duration) DockerContainerMetadata {
-	return DockerContainerMetadata{Error: unimplemented}
+func (c *containerdClient) CreateContainer(dockerConfig *docker.Config, dockerHostConfig *docker.HostConfig, name string, timeout time.Duration) DockerContainerMetadata {
+	ctx := namespaces.WithNamespace(context.TODO(), ecsNamespace)
+	image, err := c.client.GetImage(ctx, containerdImage) // TODO
+	if err != nil {
+		wrapped := errors.Wrapf(err, "containerd create: failed to resolve image %s", containerdImage)
+		return DockerContainerMetadata{Error: CannotPullContainerError{wrapped}}
+	}
+	opts := []containerd.SpecOpts{
+		containerd.WithImageConfig(ctx, image),
+		// env
+		// mounts
+		containerd.WithProcessArgs("/bin/sleep", "60"),
+	}
+	spec, err := containerd.GenerateSpec(opts...)
+	if err != nil {
+		wrapped := errors.Wrap(err, "containerd create: failed to generate spec")
+		return DockerContainerMetadata{Error: CannotPullContainerError{wrapped}}
+	}
+	id := "ecs-test-container"
+	rootfs := containerd.WithNewRootFS(id, image)
+	container, err := c.client.NewContainer(ctx, id, containerd.WithSpec(spec), containerd.WithImage(image), rootfs)
+	if err != nil {
+		wrapped := errors.Wrap(err, "containerd create: failed to create container")
+		return DockerContainerMetadata{Error: CannotPullContainerError{wrapped}}
+	}
+	seelog.Debugf("created container with id %s", container.ID())
+
+	return DockerContainerMetadata{DockerID: container.ID()}
 }
 func (c *containerdClient) StartContainer(string, time.Duration) DockerContainerMetadata {
 	return DockerContainerMetadata{Error: unimplemented}
